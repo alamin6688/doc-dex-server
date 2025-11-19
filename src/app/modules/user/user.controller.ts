a01037a4d2c428d67fd7d956694ae140f3805c8e
Alamin <alamin.kzs06@gmail.com>
2025-11-19 23:36:24 +0600

    Admin & Doctor feature added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -20,6 +20,26 @@ const createPatient = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.createAdmin(req);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Admin Created successfuly!",
+    data: result,
+  });
+});
+
+const createDoctor = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.createDoctor(req);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Doctor Created successfuly!",
+    data: result,
+  });
+});
+
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
   const filters = pick(req.query, userFilterableFields);
   const options = pick(req.query, userPaginationAndSortingFields);
@@ -37,5 +57,7 @@ const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
 
 export const UserController = {
   createPatient,
+  createAdmin,
+  createDoctor,
   getAllFromDB,
 };
