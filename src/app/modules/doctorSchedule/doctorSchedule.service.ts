230b84726f890a842416dd618f61f5594f2dbb7a
Alamin <alamin.kzs06@gmail.com>
2025-11-22 15:44:07 +0600

    Created Doctor Schedule (Part-02) with handling User Type (JWT Payload)

--- src/app/modules/doctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.service.ts
@@ -1,15 +1,28 @@
 import { prisma } from "../../shared/prisma";
+import { IJWTPayload } from "../../types/common";
 
-const insertIntoDB = async (user: any, payload: any) => {
+const insertIntoDB = async (
+  user: IJWTPayload,
+  payload: {
+    scheduleIds: string[];
+  }
+) => {
   const doctorData = await prisma.doctor.findUniqueOrThrow({
     where: {
       email: user.email,
     },
   });
 
-  
+  const doctorScheduleData = payload.scheduleIds.map((scheduleId) => ({
+    doctorId: doctorData.id,
+    scheduleId,
+  }));
 
-  return { user, payload };
+  //   console.log(doctorScheduleData);
+
+  return await prisma.doctorSchedules.createMany({
+    data: doctorScheduleData,
+  });
 };
 
 export const DoctorScheduleService = {
