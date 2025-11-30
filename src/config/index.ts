fe6fd6ceffff9111c34d2374ae8832bbb6cfcbfb
Alamin <alamin.kzs06@gmail.com>
2025-12-01 02:08:57 +0700

    Stripe Payment System Integrated With Webhook

--- src/config/index.ts
+++ src/config/index.ts
@@ -20,4 +20,7 @@ export default {
     refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
   },
   openRouterApiKey: process.env.OPENROUTER_API_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  clientUrl: process.env.CLIENT_URL,
+  webhookSecret: process.env.WEBHOOK_SECRET,
 };
