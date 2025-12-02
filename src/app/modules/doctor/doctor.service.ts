7491ca44b1458d93a0b6b7f7e6716e17573e6055
Alamin <alamin.kzs06@gmail.com>
2025-12-03 00:24:14 +0700

    Patient Review feature added

--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -68,6 +68,12 @@ const getAllFromDB = async (filters: any, options: IOptions) => {
           specialities: true,
         },
       },
+      reviews: {
+        select: {
+          rating: true,
+          comment: true,
+        },
+      },
     },
   });
 
@@ -161,6 +167,7 @@ const getByIdFromDB = async (id: string): Promise<Doctor | null> => {
           schedule: true,
         },
       },
+      reviews: true,
     },
   });
   return result;
