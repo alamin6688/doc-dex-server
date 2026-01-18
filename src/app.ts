2a985e463c8269b899cea3efb75438213162f045
Alamin <alamin.kzs06@gmail.com>
2026-01-18 17:48:40 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,11 @@ app.post(
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://doc-dex-client.vercel.app",
+    ],
     credentials: true,
   }),
 );
